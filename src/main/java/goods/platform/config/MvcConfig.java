761ac14f2a82b0ab86a82b6f33e392eaa4607d74
longyun.sun <longyun.sun@lianshang.com>
2019-11-16 17:52:14 +0800

    订单管理

--- src/main/java/goods/platform/config/MvcConfig.java
+++ src/main/java/goods/platform/config/MvcConfig.java
@@ -26,6 +26,8 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/customerList").setViewName("html/customerList");
 		registry.addViewController("/supplierList").setViewName("html/supplierList");
 		registry.addViewController("/salesOrderList").setViewName("html/salesOrderList");
+		registry.addViewController("/updateSalesOrder").setViewName("html/updateSalesOrder");
+
 		registry.addViewController("/chateauList").setViewName("html/chateauList");
 		//
 
--- src/main/java/goods/platform/controller/OrderInfoController.java
+++ src/main/java/goods/platform/controller/OrderInfoController.java
@@ -1,12 +1,18 @@
 package goods.platform.controller;
 
 import com.chisong.green.farm.app.constants.enums.Validity;
+import com.chisong.green.farm.app.dto.OrderDeliveryAddressMappingDto;
+import com.chisong.green.farm.app.dto.OrderInfoDto;
 import com.chisong.green.farm.app.example.OrderInfoExample;
 import com.chisong.green.farm.app.example.OrderInfoExample.Criteria;
+import com.chisong.green.farm.app.service.OrderDeliveryAddressMappingService;
 import com.chisong.green.farm.app.service.OrderInfoService;
 import com.lianshang.generator.commons.PageInfo;
+import goods.platform.commons.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +28,42 @@ public class OrderInfoController {
 
 	@Autowired
 	private OrderInfoService orderInfoService;
+	@Autowired
+	private OrderDeliveryAddressMappingService orderDeliveryAddressMappingService;
 
+	/**
+	 * 修改订单
+	 * @return
+	 */
+	@RequestMapping("/updateOrder")
+	public Response updateOrder(@RequestBody OrderInfoDto orderInfoDto){
+		OrderInfoDto oldOrderInfo =  orderInfoService.getOrderById(orderInfoDto.getId());
+		orderInfoService.update(orderInfoDto);
+
+		String address =  orderInfoDto.getAddress();
+		if(address == null){
+			address = "";
+		}
+		OrderDeliveryAddressMappingDto orderDeliveryAddressMappingDto =
+			orderDeliveryAddressMappingService.getById(oldOrderInfo.getOrderDeliveryAddressMappingDto().getId());
+		if(!address.equals(orderDeliveryAddressMappingDto.getAddress())){
+			orderDeliveryAddressMappingDto.setAddress(address);
+			orderDeliveryAddressMappingService.update(orderDeliveryAddressMappingDto);
+		}
+
+		return Response.success();
+	}
+
+	/**
+	 * 查询订单详情
+ 	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/detail/{id}")
+	public Response getDetail(@PathVariable Long id){
+		OrderInfoDto orderInfoDto =  orderInfoService.getOrderById(id);
+		return Response.success(orderInfoDto);
+	}
 	/**
 	 * 分页查询
 	 */
