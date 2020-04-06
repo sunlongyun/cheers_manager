bf2fdd5c3bb922a3962d85d890880262f76d340d
longyun.sun <longyun.sun@lianshang.com>
2020-04-06 13:56:38 +0800

    代码提交

--- src/main/java/goods/platform/controller/OrderInfoController.java
+++ src/main/java/goods/platform/controller/OrderInfoController.java
@@ -12,9 +12,11 @@ import com.chisong.green.farm.app.service.OrderDeliveryAddressMappingService;
 import com.chisong.green.farm.app.service.OrderInfoService;
 import com.lianshang.generator.commons.PageInfo;
 import goods.platform.commons.Response;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,22 +44,50 @@ public class OrderInfoController {
 	 * @return
 	 */
 	@RequestMapping("/updateOrder")
-	public Response updateOrder(@RequestBody OrderInfoDto orderInfoDto){
-		OrderInfoDto oldOrderInfo =  orderInfoService.getOrderById(orderInfoDto.getId());
-		orderInfoService.update(orderInfoDto);
-
-		CustomerDeliveryAddressDto customerDeliveryAddressDto =  orderInfoDto.getAddress();
+	public Response updateOrder(@RequestBody OrderInfoDto orderInfoDtoReq){
+		OrderInfoDto oldOrderInfo =  orderInfoService.getOrderById(orderInfoDtoReq.getId());
+		//覆盖
+		coverFileds(orderInfoDtoReq, oldOrderInfo);
+		if(null == oldOrderInfo.getTotalAmount() ||
+			null == oldOrderInfo.getCostAmount() ||
+			oldOrderInfo.getTotalAmount() < oldOrderInfo.getCostAmount()){
+			return Response.fail("商品售价不得低于进价");
+		}
+		oldOrderInfo.setIncome(oldOrderInfo.getTotalAmount()-oldOrderInfo.getCostAmount());
+		orderInfoService.update(oldOrderInfo);
 
 		OrderDeliveryAddressMappingDto orderDeliveryAddressMappingDto =
 			orderDeliveryAddressMappingService.getById(oldOrderInfo.getOrderDeliveryAddressMappingDto().getId());
-		orderDeliveryAddressMappingDto.setAddress(orderInfoDto.getUserAddress());
+		orderDeliveryAddressMappingDto.setAddress(orderInfoDtoReq.getUserAddress());
 
-		orderDeliveryAddressMappingDto.setAddress(orderInfoDto.getUserAddress());
-		orderDeliveryAddressMappingDto.setMobile(orderInfoDto.getMobile());
-		orderDeliveryAddressMappingDto.setContact(orderInfoDto.getCustomerName());
+		orderDeliveryAddressMappingDto.setAddress(orderInfoDtoReq.getUserAddress());
+		orderDeliveryAddressMappingDto.setMobile(orderInfoDtoReq.getMobile());
+		orderDeliveryAddressMappingDto.setContact(orderInfoDtoReq.getCustomerName());
 		orderDeliveryAddressMappingService.update(orderDeliveryAddressMappingDto);
 
 		return Response.success();
+	}
+
+	//覆盖非空字段
+	private void coverFileds(@RequestBody OrderInfoDto orderInfoDtoReq,
+		OrderInfoDto oldOrderInfo) {
+		Field[] fields = OrderInfoDto.class.getDeclaredFields();
+		for(Field f: fields){
+			if(f.getName().contains("serialVersionUID")){
+				continue;
+			}
+			try {
+				f.setAccessible(true);
+				Object value = f.get(orderInfoDtoReq);
+				if(null != value){
+					f.set(oldOrderInfo, value);
+				}
+				f.setAccessible(false);
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
+
+		}
 	}
 
 	/**
@@ -75,7 +105,7 @@ public class OrderInfoController {
 	 */
 	@RequestMapping("/getPageInfo")
 	public PageInfo getPageInfo(Integer pageNo, Integer pageSize, String orderNo,
-		String customerName, String mobile, HttpSession session) {
+		String customerName, String mobile,Integer status, HttpSession session) {
 
 		CustomerInfoDto customerInfoDto = (CustomerInfoDto) session.getAttribute("adminUser");
 
@@ -89,6 +119,9 @@ public class OrderInfoController {
 		}
 		if(!StringUtils.isEmpty(mobile)){
 			params.put("mobile", mobile);
+		}
+		if(null != status && status != -1){
+			params.put("status", status);
 		}
 
 		Integer customerId = null;
